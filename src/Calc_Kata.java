fa231285730ef0bca2afd291dc2956d83238b53d
Ruslan <ruslan.rust@gmail.com>
2023-09-07 16:27:51 +0300

    07.09_commit

--- src/Calc_Kata.java
+++ src/Calc_Kata.java
@@ -1,10 +1,25 @@
 import java.util.Scanner;
 public class Calc_Kata {
-    static Scanner read = new Scanner(System.in);
+    static Scanner in = new Scanner(System.in);
+    static char operation;
     public static void main(String[] args) {
         System.out.println("Введите выражение [2+2] или два римских числа от I до X:[V+V] + Enter ");
-        String inputData = read.nextLine();
-        System.out.print(inputData);
-
+        String inputData = in.nextLine(); //считывание строки
+        char[] sym = new char[10]; //создание символьного массива для посимвольного считывния введеной строки
+        for (int i = 0; i < inputData.length(); i++) {
+            sym[i] = inputData.charAt(i);
+            if (sym[i] == '+') {
+                operation = '+';
+            }
+            if (sym[i] == '-') {
+                operation = '-';
+            }
+            if (sym[i] == '*') {
+                operation = '*';
+            }
+            if (sym[i] == '/') {
+                operation = '/';
+            }
+        }
     }
 }
